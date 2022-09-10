11e307a371cc6ad190f77ee9978ddcef3b10825e
Raimpril <raimihassantope@gmail.com>
2022-09-10 11:58:29 -0700

    task one

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ printf("%s %d is %d and is greater than 5\n", str, n, L);
 }
 else if (L == 0)
 {
-printf("%s %d and is %d is 0\n", str, n, L);
+printf("%s %d is %d and is 0\n", str, n, L);
 }
 else if (L < 6)
 {
