a8dee19f34ceb38debf3fc10063fd508d4a8cbb0
Raimpril <raimihassantope@gmail.com>
2022-09-13 15:24:18 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print lowercase alphabets
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+char alpha = i'a';
+while (alpha <= 'z')
+{
+_putchar(alpha);
+alpha++;
+_putchar('\n')
+}
+Return (0);
+}
