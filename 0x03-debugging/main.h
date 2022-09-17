d7036ff62fe5487eea9e49770bc39a719c89da23
Raimpril <raimihassantope@gmail.com>
2022-09-16 21:52:15 -0700

    task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,9 +3,9 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int);
+void positive_or_negative(int i);
 int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_days(int, int);
 
-#endif
+#endif /* MAIN_H */
