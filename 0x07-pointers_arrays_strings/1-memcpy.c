b4fde40d85632e5597e08b569aec68eea5d42548
Raimpril <raimihassantope@gmail.com>
2022-09-26 14:10:27 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * _memset - a function that copies memory area by filling n bytes
- * @s: A pointer to the memory area to be filled
- * @c: The character to fill the memory area
+ * _memcpy - a function that copies memory area by filling n bytes
+ * @dest: A pointer to the memory area to be filled
+ * @src: The character to fill the memory area
  * @n: The number of bytes to be filled
  * Return: A pointer to the filled memory areaof s
  */
 
-void *_memset(void *s, int c, size_t n)
+void *_memcpy(void *dest, const void *src, size_t n)
 {
 unsigned int index;
-unsigned char *memory = s, value = c;
+unsigned char *destination = dest;
+const unsigned char *source = src;
 
 for (index = 0; index < n; index++)
-memory[index] = value;
-return (memory);
+destination[index] = source[index];
+return (dest);
 }
