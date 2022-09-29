bd3ce5d05a181cddd8c67c53f4459ef2939b7520
Raimpril <raimihassantope@gmail.com>
2022-09-29 13:18:51 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,4 +14,23 @@ int find_sqrt(int num, int root)
 {
 if ((root * root) == num)
 return (root);
+if (root == num / 2)
+return (-1);
+return (find_sqrt(num, root + 1));
+}
+
+/**
+ * _sqrt_recursion - Returns square root of number
+ * @n: The number to be returned
+ * Return: Returns the square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+int root = 0;
+if (n < 0)
+return (-1);
+if (n == 1)
+return (1);
+return (find_sqrt(n, root));
 }
