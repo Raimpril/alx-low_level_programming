bf8f69193439f529b4c2a478ea38a5ee369cea68
Raimpril <raimihassantope@gmail.com>
2022-09-30 16:02:17 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints its name, followed by a new line
+ * @argc: integer argument count
+ * @argv: array argument vector
+ * Return: Always return 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+(void)argc;
+return (0);
+}
