5de89018ca9c8ef84da5122a3897fa8ee54e12fd
Raimpril <raimihassantope@gmail.com>
2022-09-30 16:47:39 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies two numbers
+ * @argc: Program argument counts
+ * @argv: program argument vector
+ * Return: Always return 0
+ */
+
+int main(int argc, char *argv[])
+{
+int index, multiplication;
+multiplication = 1;
+if (argc < 3)
+{
+printf("Error\n");
+return (1);
+}
+for (index = 1; index < argc; index++)
+{
+multiplication = multiplication * atoi(argv[index]);
+}
+printf("%d\n", multiplication);
+return (0);
+}
+
