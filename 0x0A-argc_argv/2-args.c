1126abd7d4d2c3a57899bcc8650f6fcd227fe66b
Raimpril <raimihassantope@gmail.com>
2022-09-30 16:25:35 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints all arguments it receives
+ * @argc: number of arguments received
+ * @argv: array of argument vector
+ * Return: Always return success
+ */
+
+int main(int argc, char *argv[])
+{
+int n;
+for (n = 0; n < argc; n++)
+{
+printf("%s\n", argv[n]);
+}
+return (0);
+}
