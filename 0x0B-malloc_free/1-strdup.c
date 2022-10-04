80b4d3b48647118ae501a68358a9cb91d6dfb394
Raimpril <raimihassantope@gmail.com>
2022-10-04 16:00:09 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strdup - function that returns a pointer to a newly allocated
+ * _strlen - function that returns a pointer to a newly allocated
  * space in memory which contains a copy of the string given as a parameter
  * @s: Array of elements allocated
  * Return: 1
