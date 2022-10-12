c9850aa18dde109a38cb8cb61e232f1206561119
Raimpril <raimihassantope@gmail.com>
2022-10-12 16:40:03 -0700

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two intergers to get sum
+ * @a: integer 1
+ * @b: integer 2
+ * Return: sum a+b
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - Subtracts two integers to get difference
+ * @a: integer 1
+ * @b: integer 2
+ * Return: difference a-b
+ */
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two integers to get the products
+ * @a: integer 1
+ * @b: integer 2
+ * Return: product a*b
+ */
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - Divides two integers to get their value
+ * @a: integer 1
+ * @b: integer 2
+ * Return: value of a/b
+ */
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - Gets the remainder of two integers after division
+ * @a: integer 1
+ * @b: integer 2
+ * Return: modulo - remainder
+ */
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
+
