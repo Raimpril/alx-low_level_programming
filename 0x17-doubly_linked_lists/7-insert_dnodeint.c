2b4acf9d89ce5321ef8d2dcff4ded24fab20a035
Raimpril <raimihassantope@gmail.com>
2022-12-08 04:17:48 -0800

    tasks

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,6 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 {
-
 	dlistint_t *new;
 	dlistint_t *head;
 	unsigned int i;
@@ -37,7 +36,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				{
 					new = malloc(sizeof(dlistint_t));
 					if (new != NULL)
-
 					{
 						new->n = n;
 						new->next = head->next;
@@ -52,6 +50,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			i++;
 		}
 	}
-
 	return (new);
 }
