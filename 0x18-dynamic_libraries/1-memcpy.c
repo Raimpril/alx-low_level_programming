6efeeb42d5feca9b0366687598d126a7d00f32c5
Raimpril <raimihassantope@gmail.com>
2022-12-16 16:04:12 -0800

    Task 0

--- 0x18-dynamic_libraries/1-memcpy.c
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -1,21 +1,19 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
- * _memcpy - a function that copies memory area by filling n bytes
- * @dest: A pointer to the memory area to be filled
- * @src: The character to fill the memory area
- * @n: The number of bytes to be filled
- * Return: A pointer to the filled memory areaof s
+ * _memcpy - copies memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
 
-void *_memcpy(void *dest, const void *src, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int index;
-unsigned char *destination = dest;
-const unsigned char *source = src;
+	unsigned int i;
 
-for (index = 0; index < n; index++)
-destination[index] = source[index];
-return (dest);
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
+
+	return (dest);
 }
